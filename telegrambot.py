aa23153f4c005ee7bae0d030715c6688a5db79b0
NikitaED <Nik.lesinc@yandex.ru>
2025-01-17 14:36:32 +0500

    Add files via upload

---
+++ telegrambot.py
@@ -0,0 +1,24 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = '7982861761:AAFuXa_bOkC4BBTUgFI-LToqpmchA_mxVUk'
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage = MemoryStorage())
+
+
+@dp.message_handlers(commands = ['start'])
+async def start(message):
+        print('Привет! Я бот помогающий твоему здоровью.')
+        await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handlers()
+async def all_massages(message):
+        print("Введите команду /start, чтобы начать общение.")
+        await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
+
